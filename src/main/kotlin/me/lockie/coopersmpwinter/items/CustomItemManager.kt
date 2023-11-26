373aa9f726231d7cac9b3902aa2e6d0c30939b33
Sherlock Luk <sherlockluk@outlook.com>
2023-11-26 19:57:29 +0000

    fix formatting

--- src/main/kotlin/me/lockie/coopersmpwinter/items/CustomItemManager.kt
+++ src/main/kotlin/me/lockie/coopersmpwinter/items/CustomItemManager.kt
@@ -4,7 +4,9 @@ import org.bukkit.inventory.ItemStack
 import java.util.function.Consumer
 
 class CustomItemManager {
+
     var customItems = ArrayList<CustomItem>()
+
     fun getCustomItemById(id: String): ItemStack? {
         for (customItem in customItems) if (customItem.id == id) return customItem.item
         return null
